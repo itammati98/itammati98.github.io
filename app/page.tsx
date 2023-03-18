26b6c2b3b786082ba38205c92c8d16ee3de8a843
Matt <matt.wozniak98@gmail.com>
2023-03-18 22:11:34 +0100

    first commit, to consider design, and funcs

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,38 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className='flex flex-col items-center'>
+        <h1 className='px-6 text-6xl font-bold text-center'>Hi! I am <span className=' text-indigo-300'>Matt</span>!</h1>
+        <h2 className=' px-3 text-3xl font-semibold text-center mb-12'>Welcome to my playground...</h2>
+        <section className=' mb-12 rounded-3xl p-6 bg-slate-600 max-w-fit max-h-max flex flex-row flex-wrap justify-center gap-12'>
+          <Image className=' drop-shadow-md rounded-3xl' src='/avatar.png' alt='avatar image' width={500} height={500}></Image>
+          <div className='p-2 w-fit text-center flex flex-col justify-center items-center'>
+            <h3 className=' text-3xl'>I am a data analyst and a developer.</h3>
+            <p className=' max-w-md mt-3 text-xl'>I am naturally a curious learner.<br />I believe that it is equally important to stay up with the new trends and know the legacy technologies.</p>
+            <a className=' hover:cursor-pointer drop-shadow-md mt-3 rounded-xl p-3 bg-slate-500 hover:bg-slate-700 hover:duration-300'>Download my CV</a>
+          </div>
+        </section>
+        <section className=' mb-9 p-6 bg-slate-700 w-full max-h-max flex flex-col flex-wrap items-center justify-center gap-3'>
+            <h3 className=' text-center font-semibold pt-6 pb-3 text-3xl'>Favourite projects I was working on</h3>
+          <div>
+          <div className=' p-6 my-9 rounded-3xl bg-slate-600 w-fit flex flex-row flex-wrap justify-center items-center gap-12'>
+            <p className=' text-right max-w-md mt-3 text-xl'><span className=' font-semibold text-3xl'>Project 1</span><br/><br/>Description of a project. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam est ipsum quis sequi ratione dignissimos ducimus, perferendis quia, quidem sit accusantium. Delectus placeat dignissimos officia animi eos adipisci blanditiis dicta.</p>
+          <Image className='  rounded-3xl' src='/vhs.png' alt='avatar image' width={500} height={500}></Image>
+          </div>
+          <div className=' my-12 rounded-3xl bg-slate-600 p-6 w-fit flex flex-row flex-wrap justify-center items-center gap-12'>
+          <Image className=' rounded-3xl' src='/dragon.png' alt='avatar image' width={500} height={500}></Image>
+            <p className=' text-left max-w-md mt-3 text-xl'><span className=' font-semibold text-3xl'>Project 2</span><br/><br/>Description of a project. This was a very exciting project Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur perspiciatis architecto dignissimos exercitationem. Sapiente, magni voluptatum aut deleniti iste voluptatibus ullam quis, nam pariatur nisi iure, corrupti delectus soluta odio.</p>
+          </div>
+          <div className='my-12 rounded-3xl bg-slate-600 p-6 w-fit text-center flex flex-row flex-wrap justify-center items-center gap-12'>
+            <p className=' text-right max-w-md mt-3 text-xl'><span className=' font-semibold text-3xl'>Project 3</span><br/><br/>Description of a project. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident consequuntur aliquam voluptatibus possimus architecto repellat, quia omnis in culpa ipsam blanditiis quos, magni sunt. Vel obcaecati consectetur eum fuga distinctio.</p>
+          <Image className='rounded-3xl' src='/singing.png' alt='avatar image' width={500} height={500}></Image>
+          </div>
+          </div>
+        </section>
     </main>
   )
 }
---
+++ app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function Projects(){
+    return(
+        <div>
+            <h1>Page for projects</h1>
+        </div>
+    )
+}
