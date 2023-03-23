2fec911df0d1c59f5c6c7826bf7ef7bb5477df51
Matt <matt.wozniak98@gmail.com>
2023-03-23 21:53:29 +0100

    responsive nav + metadata for projects

--- app/experimental/page.tsx
+++ app/experimental/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Projects',
+    description: 'Browse all the projects I have created and wanted to share in my portfolio.'
+}
+
 export default function Experimental(){
     return(
         <div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <body className='bg-neonCity bg-cover bg-fixed  text-white scroll-smooth'>
       <div className=' bg-glass fixed top-0 left-0 z-50 w-screen flex flex-row justify-center py-3 duration-300'>
-        <ul className=' text-2xl flex flex-row gap-6 max-w-4xl'>
+        <ul className=' lg:text-2xl text-lg flex flex-row gap-6 max-w-4xl'>
           <li><a className='p-2 hover:text-indigo-300 hover:duration-300' href="/">Home</a></li>
           <li><a className='p-2 hover:text-indigo-300 hover:duration-300' href="/projects">Projects</a></li>
           <li><a className='p-2 hover:text-indigo-300 hover:duration-300' href="/experimental">Experimental</a></li>
